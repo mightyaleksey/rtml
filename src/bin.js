b2a429306df82e9213cf809d63c99a84874ee182
Alexey Litvinov <sullenor@yandex-team.ru>
2018-04-22 20:05:42 +0300

    initial implementation

---
+++ src/bin.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+'use strict';
+
+const {name, version} = require('../package');
+process.title = name;
+
+const USAGE = `
+  Usage: rtml <file> [OPTIONS]
+
+  Options:
+
+    -d, --dir      change output dir
+    -h, --help     print usage
+    -s, --styled   inject css generated via styled-components
+    -v, --version  print version
+    -w, --watch    use watch mode
+
+  Examples:
+
+    rtml src/pages/index.js
+    rtml src/pages/*.js -d dist -w
+`;
+
+const NOCOMMAND = `
+  Provide any file:
+
+    rtml src/pages/index.js
+`;
+
+const minimist = require('minimist');
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    dir: 'd',
+    help: 'h',
+    styled: 's',
+    version: 'v',
+    watch: 'w',
+  },
+  boolean: [
+    'help',
+    'styled',
+    'version',
+    'watch',
+  ],
+  string: [
+    'dir',
+  ],
+  default: {
+    dir: process.cwd(),
+  },
+});
+
+if (argv.version) {
+  console.log(version);
+  process.exit();
+}
+
+if (argv.help) {
+  console.log(USAGE);
+  process.exit();
+}
+
+if (argv._.length === 0) {
+  console.log(NOCOMMAND);
+  process.exit();
+}
+
+const fg = require('fast-glob');
+const fs = require('fs');
+const path = require('path');
+const render = require('./render');
+
+const dirname = path.resolve(argv.dir);
+const stream = fg.stream(argv._, { absolute: true });
+
+stream.on('data', task);
+stream.once('error', console.error);
+stream.once('end', () => {
+  if (argv.watch) {
+    const chokidar = require('chokidar');
+    const watcher = chokidar.watch(argv._, {
+      persistent: true,
+      cwd: process.cwd(),
+    });
+
+    watcher.on('change', task);
+  }
+});
+
+function task(filepath) {
+  const destpath = htmlpath(filepath, dirname);
+  return render(filepath, argv.styled)
+    .then(html => write(destpath, html))
+    .catch(error => console.error(error));
+}
+
+function htmlpath(filepath, dir) {
+  const filename = path.basename(filepath);
+  const htmlname = filename.replace(/\.\w+$/, '') + '.html';
+  return path.join(dir, htmlname);
+}
+
+function write(filepath, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filepath, data, 'utf8', error => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(filepath);
+      }
+    });
+  });
+}
---
+++ src/render.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+const doctype = '<!doctype html>';
+
+module.exports = render;
+
+function loadComponent(filepath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const Component = require(filepath);
+      resolve(typeof Component.default === 'function'
+        ? Component.default
+        : Component);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function renderComponent(Component) {
+  const element = React.createElement(Component);
+  const html = ReactDOMServer.renderToStaticMarkup(element);
+  return `${doctype}${html}`;
+}
+
+function renderStyledComponent(Component) {
+  const element = React.createElement(Component);
+
+  const { ServerStyleSheet } = require('styled-components');
+  const sheet = new ServerStyleSheet();
+  const html = ReactDOMServer.renderToStaticMarkup(sheet.collectStyles(element));
+
+  const styleTags = sheet.getStyleTags();
+  const cheerio = require('cheerio');
+  const $ = cheerio.load(html);
+  $('head').append(styleTags);
+
+  return `${doctype}${$.html()}`;
+}
+
+function render(filepath, useSC) {
+  const promise = loadComponent(filepath);
+  if (useSC) return promise.then(renderStyledComponent);
+  return promise.then(renderComponent);
+}
